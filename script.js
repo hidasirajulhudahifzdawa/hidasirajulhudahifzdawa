f52845b08da92c4ed56e71e443a1ae0ee263e3e0
hidasirajulhudahifzdawa <muhammedasim711@gmail.com>
2024-11-23 19:01:05 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("payNow").addEventListener("click", function () {
-    const upiLink = "upi://pay?pa=muhammedasim711@oksbi&pn=Muhammed%20Asim&am=100&cu=INR&tn=Debt%20Payment";
+    const upiLink = "upi://pay?pa=muhammedasim711@oksbi&pn=Muhammed Asim&am=100&cu=INR&tn=Debt Payment";
     window.location.href = upiLink;
   
-    // Mock payment success (for demonstration purposes)
+    // Mock payment success
     setTimeout(() => {
-        document.getElementById("successMessage").classList.remove("hidden");
+      document.getElementById("successMessage").classList.remove("hidden");
     }, 3000);
-});
+  });
