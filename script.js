768d0155952f0278b5edea7a12c571a21c437489
hidasirajulhudahifzdawa <muhammedasim711@gmail.com>
2024-11-25 19:26:06 +0530

    script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ payments.forEach((payment) => {
     card.className = "card";
 
     // Corrected UPI URL format
-    const upiUrl = `upi://pay?pa=${encodeURIComponent(payment.upiId)}&pn=${encodeURIComponent(payment.name)}&am=${payment.debt}&cu=INR&tn=${encodeURIComponent('Payment for Exam Fee')}`;
+    const upiUrl = `upi://pay?pa=<payee UPI ID>&pn=<payee name>&am=<amount>&cu=INR&tn=<transaction note>`;
     // Add content to the card
     card.innerHTML = `
       <h3>${payment.name}</h3>
